d071056e9317c3c9fc2fef5e1e7971a73ac1fa57
duyhaule2001 <duyhaule2001@gmail.com>
2024-11-02 09:22:56 +0900

    Java Annotation, disable security

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,17 +2,13 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext leduyhau = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : leduyhau.getBeanDefinitionNames()) {
-			System.out.println(s);
-
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
 	}
 }
