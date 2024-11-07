c296a6b8706eae016a1544749ad58c508eef5266
duyhaule2001 <duyhaule2001@gmail.com>
2024-11-07 17:01:35 +0900

    #60. Repository

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,8 +21,7 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("hau", test);
+        model.addAttribute("hau", "test");
         model.addAttribute("leduyhau", "form controller");
         return "leduyhau";
     }
@@ -36,6 +36,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User leduyhau) {
         System.out.println("run here" + leduyhau);
+        this.userService.handleSaveUser(leduyhau);
         return "leduyhau";
     }
 
